c455c3549679e957735102a618893d061e2961da
Joey Fasanelli <97641189+joeyfasanelli@users.noreply.github.com>
2025-09-11 09:17:55 -0400

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,7 +19,7 @@ function Projects({}: Props) {
                 <div className="w-[500px] p-20 flex-shrink-0 snap-center flex flex-col items-center justify-center">
                     <a href="https://kadostudy.netlify.app/" target="_blank">
                     <img
-                    className="rounded-md"
+                    className="rounded-md h-[175px]"
                     src="https://i.imgur.com/6F55FxK.png"
                     alt=""
                     />
